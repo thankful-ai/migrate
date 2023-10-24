2ba7aebf19d1dbe4c93e874a2ed3e3caec446d0c
albertrdixon <adixon415@gmail.com>
2023-10-24 11:57:20 -0700

    scripts/docker-entrypoint: improve certificate env var names

--- scripts/docker-entrypoint.sh
+++ scripts/docker-entrypoint.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
-if test -n "$CLIENT_CERT_B64"; then
-  echo "$CLIENT_CERT_B64" | base64 -d >/tmp/client-cert.pem
+if test -n "$DATABASE_CLIENT_CERT_B64"; then
+  echo "$DATABASE_CLIENT_CERT_B64" | base64 -d >/tmp/client-cert.pem
 fi
 
-if test -n "$CLIENT_KEY_B64"; then
-  echo "$CLIENT_KEY_B64" | base64 -d >/tmp/client-key.pem
+if test -n "$DATABASE_CLIENT_KEY_B64"; then
+  echo "$DATABASE_CLIENT_KEY_B64" | base64 -d >/tmp/client-key.pem
 fi
 
-if test -n "$CA_B64"; then
-  echo "$CA_B64" | base64 -d >/tmp/ca.pem
+if test -n "$DATABASE_SERVER_CA_B64"; then
+  echo "$DATABASE_SERVER_CA_B64" | base64 -d >/tmp/ca.pem
 fi
 
 exec /migrate "$@"
